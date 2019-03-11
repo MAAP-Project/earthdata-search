5c48e3ebc4f1c773befc9fdffa6261a7214c830d
Aimee Barciauskas <aimee@developmentseed.org>
2019-03-11 10:45:21 -0400

    Remove references to 'Include non-EOSDIS collections'

--- spec/features/collections/collection_include_eosdis_filtering_spec.rb
+++ spec/features/collections/collection_include_eosdis_filtering_spec.rb
@@ -1,36 +1,6 @@
 require 'rails_helper'
 
 describe 'Collection include EOSDIS filtering' do
-  context "When searching collections with 'Include non-EOSDIS collections' filter unchecked" do
-    before :all do
-      load_page :search, ac: true
-
-      @all_collections_count = collection_results_header_value.text.to_i
-
-      find(:css, "#hasNonEOSDIS").set(false)
-      wait_for_xhr
-    end
-
-    it 'decreases the number of collections returned' do
-      only_eosdis_collections = collection_results_header_value.text.to_i
-      expect(@all_collections_count).to be > only_eosdis_collections
-    end
-
-    context "When checking 'Include non-EOSDIS collections'" do
-      before :all do
-        @only_eosdis_collections = collection_results_header_value.text.to_i
-
-        find(:css, "#hasNonEOSDIS").set(true)
-        wait_for_xhr
-      end
-
-      it 'increases the number of collections returned' do
-        all_collections_count = collection_results_header_value.text.to_i
-        expect(all_collections_count).to be > @only_eosdis_collections
-      end
-    end
-  end
-
   context "when selecting a facet filter while searching collections with 'Include non-EOSDIS collections' filter unchecked" do
     before :all do
       load_page :search, facets: true, ac: true
